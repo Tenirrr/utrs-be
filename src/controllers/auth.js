3f090b90f7ca6777d9513c2bd20ade4956bf6250
Tenirrr <112577083+Tenirrr@users.noreply.github.com>
2025-01-10 16:48:02 +0100

    comments

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,23 +1,39 @@
 const signin = async (req, res) => {
     try {
+        if (req.session.views) {
+            req.session.views++;
+            return res.send(`Liczba odwiedzin: ${req.session.views}`);
+          } else {
+            req.session.views = 1;
+            return res.send('Witaj po raz pierwszy! Odśwież stronę, aby zwiększyć licznik.');
+          }
         return res.status(200).json({
             code: 200,
             message: "Dziala"
         });
     } catch (error) {
-        
+        console.error("Auth controller (setup2fa) error: " + error);
+        return res.status(500).json({
+            code: 500,
+            message: "Błąd po stronie serwera!"
+        });
     }
 }
 
-const signout = async (req, res) => {
-    try {
+const signout = (req, res) => {
+    req.session.destroy((error) => {
+        if (error) {
+            return res.status(500).json({
+                code: 500,
+                message: "Błąd po stronie serwera!"
+            });
+        }
+        res.clearCookie('connect.sid');
         return res.status(200).json({
             code: 200,
-            message: "Dziala"
+            message: "OK"
         });
-    } catch (error) {
-        
-    }
+    });
 }
 
 const signup = async (req, res) => {
--- src/routes/error.js
+++ src/routes/error.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const errorCtrl = require("../controllers/error.js")
 
-router.get("/404", errorCtrl.http404);
+// Handling error http 404
+router.all("/404", errorCtrl.http404);
 
 module.exports = router;
